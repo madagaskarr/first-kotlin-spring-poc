6e654f3ba8ff65bcc156ae68a8f0e1e9d9ca93b3
tigran <me@tigranes.io>
2020-08-23 20:54:01 -0700

    Initial commit

---
+++ src/main/kotlin/me/tigranes/firstkotlinspringpoc/FirstKotlinSpringPocApplication.kt
@@ -0,0 +1,11 @@
+package me.tigranes.firstkotlinspringpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKotlinSpringPocApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstKotlinSpringPocApplication>(*args)
+}
